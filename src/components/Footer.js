e29abc8384075d00cd72f6c1255c2034ffd2cab6
Meech <meech@dannys-mbp.home>
2023-06-14 20:58:01 -0400

    made new repo and trasferred over old code

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer () {
+    return (
+        <footer >
+          <a class="links" href='https://github.com/Danny2727'> <i class="fa-brands fa-github fa-spin fa-spin-reverse fa-2xl"></i></a>
+           <a class ="links" href='www.linkedin.com/in/daniel-edouard'><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+        </footer>
+    )
+}
